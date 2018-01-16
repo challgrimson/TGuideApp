80eb8162ef70774207954f84ca76b4d6e918606b
Christian <christianhallgrimson@yahoo.ca>
2018-01-15 19:22:48 -0500

    Large FAQ update of barebones with working button/text animation

--- app/src/main/java/com/example/android/tguide/FAQ.java
+++ app/src/main/java/com/example/android/tguide/FAQ.java
@@ -1,18 +1,26 @@
 package com.example.android.tguide;
-
+// Code for questions
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.transition.ChangeBounds;
+import android.support.transition.TransitionManager;
+import android.support.transition.TransitionSet;
 import android.support.v4.app.Fragment;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link FAQ.OnFragmentInteractionListener} interface
+ * {@link OnFragmentInteractionListener} interface
  * to handle interaction events.
  * Use the {@link FAQ#newInstance} factory method to
  * create an instance of this fragment.
@@ -63,15 +71,105 @@ public class FAQ extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_faq, container, false);
+        final View view = inflater.inflate(R.layout.fragment_faq, container, false);
 
-        // Send FAQ as a title parameter
-        if (mListener != null){
+        // Send Main Page as a title parameter
+        if (mListener != null) {
             mListener.onFragmentInteraction("FAQ");
         }
 
+        //current implementation of setting clicking for FAQ buttons, sure will be changed
+        //------
+        final Button btn1 = (Button) view.findViewById(R.id.button1);
+        final Button btn2 = (Button) view.findViewById(R.id.button2);
+        final Button btn3 = (Button) view.findViewById(R.id.button3);
+
+        final TextView tv1 = (TextView) view.findViewById(R.id.text1);
+
+        final Button[] btnArray = {btn1, btn2, btn3};
+        final TextView[] tvArray = {tv1};
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                buttonResponse(1, btnArray, tvArray, view);
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                buttonResponse(2, btnArray,tvArray,view);
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                buttonResponse(3, btnArray,tvArray,view);
+            }
+        });
+
+
         return view;
     }
+
+    boolean[] posFlag = {false, false, false};
+    boolean[] isCollapsed = {false};
+
+    public void buttonResponse(int id, Button[] btnArray, final TextView[] tvArray, View view) {
+        float deltaX;
+        int[] location = new int[2];
+
+
+
+        for (int i = id; i < btnArray.length; i++) {
+            btnArray[i].getLocationInWindow(location);
+
+            if (posFlag[id] == true)
+                location[1] -= 940;
+
+            ObjectAnimator animX = ObjectAnimator.ofFloat(btnArray[i], "y", location[1]).setDuration(300);
+            animX.start();
+
+        }
+
+
+        if (posFlag[id] == false) {
+            posFlag[id] = true;
+        } else {
+            posFlag[id] = false;
+        }
+
+        if(!isCollapsed[0]){
+            ValueAnimator va = ValueAnimator.ofInt(0, 432);
+            va.setDuration(300);
+            va.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    Integer value = (Integer) animation.getAnimatedValue();
+                    tvArray[0].getLayoutParams().height = value.intValue();
+                    tvArray[0].requestLayout();
+                }
+            });
+            va.start();
+            isCollapsed[0] = true;
+        }
+        else{
+            ValueAnimator va = ValueAnimator.ofInt(432, 0);
+            va.setDuration(300);
+            va.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    Integer value = (Integer) animation.getAnimatedValue();
+                    tvArray[0].getLayoutParams().height = value.intValue();
+                    tvArray[0].requestLayout();
+                }
+            });
+            va.start();
+            isCollapsed[0] = false;
+        }
+    }
+
+
+
+
+
 
     @Override
     public void onAttach(Context context) {
