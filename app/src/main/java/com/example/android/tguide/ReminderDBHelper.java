f6e4911427f268a8cbf54719c6ca42b2d42ec5b8
Christian <christianhallgrimson@yahoo.ca>
2018-05-21 12:49:32 -0400

    reminderDBhelper added firebase support

--- app/src/main/java/com/example/android/tguide/ReminderDBHelper.java
+++ app/src/main/java/com/example/android/tguide/ReminderDBHelper.java
@@ -7,6 +7,14 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 // To put in database
 /**
@@ -67,6 +75,20 @@ public class ReminderDBHelper extends SQLiteOpenHelper {
                 SURV_UNIQE_ID + " TEXT NOT NULL, " +
                 SURV_ALARM_TIME + " TEXT NOT NULL)"
         );
+
+        FirebaseDatabase.getInstance().getReference().child("usersRem")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                            usersRem user = snapshot.getValue(usersRem.class);
+                            insertReminderFromFire(user.gettitleText(),user.getdescription(), user.getdateText(), user.gettimeText(), user.getrepeat(), user.getrepeatNum(), user.getrepeatType(), user.getsound(), user.getuniqueID());
+                        }
+                    }
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                    }
+                });
     }
 
     @Override
@@ -93,6 +115,43 @@ public class ReminderDBHelper extends SQLiteOpenHelper {
 
         // Insert into database
         long reminderID = db.insert(REMINDER_TABLE_NAME, null, contentValues);
+
+        //add to firebase
+        //titletext is the header of which rest of info is under
+        FirebaseUser userr = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference reff = FirebaseDatabase.getInstance().getReference();
+        reff.child("usersRem").child(userr.getUid()).child(titleText).child("titleText").setValue(titleText);
+        reff.child("usersRem").child(userr.getUid()).child(titleText).child("description").setValue(description);
+        reff.child("usersRem").child(userr.getUid()).child(titleText).child("dateText").setValue(dateText);
+        reff.child("usersRem").child(userr.getUid()).child(titleText).child("timeText").setValue(timeText);
+        reff.child("usersRem").child(userr.getUid()).child(titleText).child("repeat").setValue(repeat);
+        reff.child("usersRem").child(userr.getUid()).child(titleText).child("repeatNum").setValue(repeatNum);
+        reff.child("usersRem").child(userr.getUid()).child(titleText).child("repeatType").setValue(repeatType);
+        reff.child("usersRem").child(userr.getUid()).child(titleText).child("sound").setValue(sound);
+        reff.child("usersRem").child(userr.getUid()).child(titleText).child("uniqueID").setValue(uniqueID);
+
+        return reminderID;
+    }
+
+    // Insert Reminder into database from firebase
+    //just no adding to firebase afterwards
+    public long insertReminderFromFire(String titleText, String description, String dateText, String timeText, String repeat,
+                               String repeatNum, String repeatType, String sound, String uniqueID) {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(REMINDER_COLUMN_TITLE, titleText);
+        contentValues.put(REMINDER_COLUMN_DESCRIPTION, description);
+        contentValues.put(REMINDER_COLUMN_DATE, dateText);
+        contentValues.put(REMINDER_COLUMN_TIME, timeText);
+        contentValues.put(REMINDER_COLUMN_REPEAT, repeat);
+        contentValues.put(REMINDER_COLUMN_REPEATNUM, repeatNum);
+        contentValues.put(REMINDER_COLUMN_REPEATTYPE, repeatType);
+        contentValues.put(REMINDER_COLUMN_SOUND, sound);
+        contentValues.put(REMINDER_UNIQUE_ID, uniqueID);
+
+        // Insert into database
+        long reminderID = db.insert(REMINDER_TABLE_NAME, null, contentValues);
+
         return reminderID;
     }
 
@@ -132,6 +191,17 @@ public class ReminderDBHelper extends SQLiteOpenHelper {
 
         // Insert into database
         db.update(REMINDER_TABLE_NAME, contentValues, REMINDER_COLUMN_ID + " = ?", new String[] { Integer.toString(reminderID) } );
+
+        FirebaseUser userr = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference reff = FirebaseDatabase.getInstance().getReference();
+        reff.child("usersRem").child(userr.getUid()).child(titleText).child("titleText").setValue(titleText);
+        reff.child("usersRem").child(userr.getUid()).child(titleText).child("description").setValue(description);
+        reff.child("usersRem").child(userr.getUid()).child(titleText).child("dateText").setValue(dateText);
+        reff.child("usersRem").child(userr.getUid()).child(titleText).child("timeText").setValue(timeText);
+        reff.child("usersRem").child(userr.getUid()).child(titleText).child("repeat").setValue(repeat);
+        reff.child("usersRem").child(userr.getUid()).child(titleText).child("repeatNum").setValue(repeatNum);
+        reff.child("usersRem").child(userr.getUid()).child(titleText).child("repeatType").setValue(repeatType);
+        reff.child("usersRem").child(userr.getUid()).child(titleText).child("sound").setValue(sound);
         return true;
     }
 
@@ -160,6 +230,8 @@ public class ReminderDBHelper extends SQLiteOpenHelper {
             contentValues.put(SURV_UNIQE_ID, uniqueid);
             // Insert into database
             db.insert(SURV_TABLE_NAME, null, contentValues);}
+
+
     }
 
     // Check to see if alread inserted
